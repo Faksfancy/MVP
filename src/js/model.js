514f1a6b51021560dc4ab4fd680e51e5bfb079f7
Faksfancy <ayfakolujo@gmail.com>
2024-07-08 19:18:06 +0100

    refactoring for MVC, helper and config file implementing error and success messages

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    console.log(data);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_Url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(res, data, recipe);
+  } catch (err) {
+    //handling error
+    throw err;
+  }
+};
